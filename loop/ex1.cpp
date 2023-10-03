e5016c5eb5ce72aa6cbbe82e57f0d297826e284a
tran ky hung <trankyhung225@gmail.com>
2023-10-03 20:09:03 +0700

    first commit

---
+++ loop/ex1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++>
+using namespace std;
+/*
+Tính tổng ước của số nguyên dương n. Bài này duyệt từ 1 tới n sẽ bị quá thời gian cho phép. 
+Sample Input 0
+28
+Sample Output 0
+56
+*/
+int main(){
+	
+}
