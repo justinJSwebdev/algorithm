57dce1fd1945500f735b1454c3c4b1bcda07dcf5
tran ky hung <trankyhung225@gmail.com>
2023-10-04 10:09:52 +0700

    ex 2 3 4 5

---
+++ loop/ex3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+
+Liệt kê các số chính phương dương và không vượt quá n
+
+Ví dụ N = 20 => 1, 4, 9, 16
+
+Gợi ý : Duyệt các số tự nhiên i <= căn N, khi đó i * i sẽ là số chính phương và sẽ <= N
+
+Constraints
+1≤N≤10^10.
+
+Tính tổng ước của số nguyên dương n. Bài này duyệt từ 1 tới n sẽ bị quá thời gian cho phép. 
+
+Input Format
+
+Số nguyên dương n
+
+Constraints
+
+1≤n≤10^10.
+
+Sample Input 0
+
+50
+Sample Output 0
+
+1 4 9 16 25 36 49
+*/
+	int main(){
+	
+	}
---
+++ loop/ex5.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+//	Kiểm tra số 2022
+/*
+Nhập vào 1 dãy số có không quá 10000 số nguyên. Hãy xác định trong quá trình nhập có xuất hiện số 2022 hay không?
+Constraints
+
+1≤N≤10000; Các số được nhập là số nguyên không quá 10^6.
+
+Output Format
+
+In YES nếu trong các số vừa nhập có số 2022, ngược lại in NO
+
+Sample Input 0
+
+4
+2019 2020 2021 2022
+Sample Output 0
+
+YES
+
+*/
+int main(){
+
+}
