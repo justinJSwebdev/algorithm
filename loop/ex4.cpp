9f37c9641cff0fc26472a03eb291239c365decb3
tran ky hung <trankyhung225@gmail.com>
2023-10-04 10:42:00 +0700

    ex 2-3-4-5 loop ans

--- loop/ex4.cpp
+++ loop/ex4.cpp
@@ -11,12 +11,21 @@ Output Format
 Tích các ước số của N
 
 Sample Input 0
-
 10
 Sample Output 0
-
 100
 */
 int main(){
-
+	int n;
+	long long tich = 1;
+	cin>>n;
+	for(int i = 1 ; i <= sqrt(n); i++){
+		if(n%i==0){
+			tich*=i;
+			if(n/i!=i){
+				tich*=n/i;
+			}
+		}
+	}
+	cout<<tich;
 }
